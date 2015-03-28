4d8613448631abc475f05f17a24b7c701a38da60
robin hood <fordarnold@gmail.com>
2015-03-29 02:34:33 +0300

    Reorganising the cide

--- Library.js
+++ Library.js
@@ -1,17 +1,20 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+  // define a list of books
+  Template.bookShelf.rendered = function() {
+    Session.setDefault('books', [
+      {title: "To Kill a Mockingbird", author: "Harper Lee"},
+      {title: "1984", author: "George Orwell"},
+      {title: "The Lord of the Rings", author: "J. R. R. Tolkien"},
+      {title: "The Catcher in the Rye", author: "J. D. Salinger"},
+      {title: "The Great Gatsby", author: "F. Scott Fitzgerald"}
+    ]);
+  };
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  // pass session variable to template
+  Template.bookShelf.helpers({
+    books: function () {
+      return Session.get('books');
     }
   });
 }
